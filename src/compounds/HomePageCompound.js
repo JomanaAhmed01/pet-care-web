472fb57adaef415c767a26b6b048c782f6752646
Jomana ahmed <ojy.ahmed@outlook.com>
2024-06-27 15:55:01 +0200

    Adding Navbar.js file to the HomePageCompound.js file & Changing the background-color

--- src/compounds/HomePageCompound.js
+++ src/compounds/HomePageCompound.js
@@ -1,16 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "../components/HomePage/Navbar";
 
 function HomePageCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.div`
-  
+  background-color: #ACD5DE;
 `;
 
 export default HomePageCompound;
